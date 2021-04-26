ccd0747dd2a75a781a2c0e64b9dc985e0015738a
baldwinrock44 <44527590+baldwinrock44@users.noreply.github.com>
2021-04-26 07:57:32 -0400

    Add files via upload

---
+++ project-lionden-api/backend/project-lionden.Domain.Tests/project-lionden-api/backend/project.lionden.Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using project.lionden.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+ 
+namespace project.lionden.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+        : base(options)
+        { }
+ 
+        public DbSet<Item> Items {get; set; }
+        public DbSet<Order> Orders {get; set;}
+    }
+
+}
